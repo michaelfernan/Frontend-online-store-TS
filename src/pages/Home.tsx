6934d1481996607d6eb833ea31894e18a9421eb2
ClaraPessanha <clara.pessanha@yahoo.com.br>
2023-09-20 07:47:44 -0300

    feat: fix lint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { getProductsFromSearch,
   getCategories, getProductsFromCategory } from '../services/api';
 import { Category } from '../types';
